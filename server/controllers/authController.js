a7bc2741cccfe0eb50e0ca495359777a012d7124
moldir <moldirshb@gmail.com>
2022-12-22 18:14:57 -0600

    auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 const { DATABASE_CONFIG } = process.env;
 const Sequelize = require("sequelize");
 
@@ -11,3 +11,36 @@ const sequelize = new Sequelize(DATABASE_CONFIG, {
     },
   },
 });
+module.exports = {
+  register: (req, res) => {
+    const { email, password } = req.body;
+        sequelize.query(`select * from users where email = '${email}'`)
+            .then((dbRes) => {
+      res.status(200).send(dbRes[0]);
+    });
+  },
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    const db = req.app.get("db");
+
+    const [foundUser] = await db.users.check_user({ email });
+    if (!foundUser) {
+      return res
+        .status(401)
+        .send("Account not found.Try a different email or register");
+    }
+
+    const authenticated = bcrypt.compareSync(password, foundUser.password);
+    if (!authenticated) {
+      res.status(403).send("Incorrect password");
+    }
+    delete foundUser.password;
+    req.session.user = foundUser;
+    // console.log(req.session.user)
+    res.status(202).send(req.session.user);
+  },
+  logout: async (req, res) => {
+    req.session.destroy();
+    res.sendStatus(200);
+  },
+};
--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,4 +1,6 @@
+
 require("dotenv").config();
+
 
 const { DATABASE_CONFIG } = process.env;
 const Sequelize = require("sequelize");
@@ -15,7 +17,9 @@ const sequelize = new Sequelize(DATABASE_CONFIG, {
 module.exports = {
   getUsers: (req, res) => {
     sequelize.query(`select * from users`).then((dbRes) => {
-      res.status(200).send(dbRes[0]);
-    });
-  },
-};
+       res.status(200).send(dbRes[0]);
+    })
+   
+}
+  }
+  
