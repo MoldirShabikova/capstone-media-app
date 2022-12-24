3c429d5ad0a243ea2525c77650ed8c0998174ec4
moldir <moldirshb@gmail.com>
2022-12-24 00:37:14 -0600

    auth working on backend

--- server/index.js
+++ server/index.js
@@ -1,49 +1,28 @@
-// require packages
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-
-const  userCtrl  = require("./controllers/usersController");
-// const authCtrl  = require("./controllers/authController");
-// const  commentCtrl  = require("./controllers/commentsController");
-const  postCtrl  = require("./controllers/postsController");
-// const  likesCtrl  = require("./controllers/likeController");
-
-
-const { SERVER_PORT, DATABASE_CONFIG } = process.env;
-
-//App instance
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+import userRoutes from "./routes/users.js"
+import authRoutes from "./routes/auth.js"
+import postRoutes from "./routes/posts.js"
+import commentRoutes from "./routes/comments.js"
+import likeRoutes from "./routes/likes.js"
 
-//Middleware
-app.use(express.json());
-app.use(cors());
-app.use(express.static("public"));
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
 
 
-//entry-point for app
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+app.get("/test", (req, res) => {
+  res.send("it works");
 });
 
-//Auth Endpoints
-// app.post("/auth/register", authCtrl.register);
-// app.post("/auth/login", authCtrl.login);
-// app.get("/auth/logout", authCtrl.logout);
-// app.get("/auth/me", authCtrl.getUser);
 
-
-//Posts Endpoints
-app.get("/api/getPosts", postCtrl.getPosts);
-
-//Users Endpoints
-app.get("/api/users", userCtrl.getUsers);
-
-//Comments Endpoints
-// app.get("/api/comments", getComments)
-
-//Likes Endpoints
-// app.get("/api/likes", getLikes)
-
-app.listen(SERVER_PORT, () => console.log("listening on port ${SERVER_PORT}"));
+app.listen(4000, () => console.log("listening on port 4000"));
